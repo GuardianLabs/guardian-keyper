7c10908d26dce48c13813259c6f91a1d8174a36b
Postnikov Aleksander <alexandrim0@gmail.com>
2023-01-05 00:11:21 +0300

    - remove seeds

--- lib/src/core/model/key_bunch.dart
+++ lib/src/core/model/key_bunch.dart
@@ -5,20 +5,16 @@ class KeyBunch extends Serializable {
   static const currentVersion = 1;
   static const aesKeyLength = 32;
 
-  final Uint8List encryptionSeed,
-      encryptionPublicKey,
+  final Uint8List encryptionPublicKey,
       encryptionPrivateKey,
-      signSeed,
       signPublicKey,
       signPrivateKey,
       encryptionAesKey;
 
   @override
   List<Object> get props => [
-        encryptionSeed,
         encryptionPublicKey,
         encryptionPrivateKey,
-        signSeed,
         signPublicKey,
         signPrivateKey,
         encryptionAesKey,
@@ -26,10 +22,8 @@ class KeyBunch extends Serializable {
 
   @override
   bool get isEmpty =>
-      encryptionSeed.isEmpty &&
       encryptionPublicKey.isEmpty &&
       encryptionPrivateKey.isEmpty &&
-      signSeed.isEmpty &&
       signPublicKey.isEmpty &&
       signPrivateKey.isEmpty &&
       encryptionAesKey.isEmpty;
@@ -38,10 +32,8 @@ class KeyBunch extends Serializable {
   bool get isNotEmpty => !isEmpty;
 
   const KeyBunch({
-    required this.encryptionSeed,
     required this.encryptionPublicKey,
     required this.encryptionPrivateKey,
-    required this.signSeed,
     required this.signPublicKey,
     required this.signPrivateKey,
     required this.encryptionAesKey,
@@ -53,10 +45,8 @@ class KeyBunch extends Serializable {
     switch (version) {
       case 1:
         return KeyBunch(
-          encryptionSeed: Uint8List.fromList(u.unpackBinary()),
           encryptionPublicKey: Uint8List.fromList(u.unpackBinary()),
           encryptionPrivateKey: Uint8List.fromList(u.unpackBinary()),
-          signSeed: Uint8List.fromList(u.unpackBinary()),
           signPublicKey: Uint8List.fromList(u.unpackBinary()),
           signPrivateKey: Uint8List.fromList(u.unpackBinary()),
           encryptionAesKey: Uint8List.fromList(u.unpackBinary()),
@@ -70,10 +60,8 @@ class KeyBunch extends Serializable {
   Uint8List toBytes() {
     final p = Packer()
       ..packInt(currentVersion)
-      ..packBinary(encryptionSeed)
       ..packBinary(encryptionPublicKey)
       ..packBinary(encryptionPrivateKey)
-      ..packBinary(signSeed)
       ..packBinary(signPublicKey)
       ..packBinary(signPrivateKey)
       ..packBinary(encryptionAesKey);
