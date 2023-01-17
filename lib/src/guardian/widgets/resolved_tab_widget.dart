d58040c820765ffa2d2bbdf902838a0d982d3207
Postnikov Aleksander <alexandrim0@gmail.com>
2023-01-17 07:18:15 +0300

    - extract widgets

---
+++ lib/src/guardian/widgets/resolved_tab_widget.dart
@@ -0,0 +1,39 @@
+import '/src/core/theme/theme.dart';
+import '/src/core/widgets/common.dart';
+import '/src/core/di_container.dart';
+import '/src/core/model/core_model.dart';
+
+import 'message_list_tile.dart';
+
+class ResolvedTabWidget extends StatelessWidget {
+  const ResolvedTabWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) =>
+      ValueListenableBuilder<Box<MessageModel>>(
+        valueListenable: context.read<DIContainer>().boxMessages.listenable(),
+        builder: (context, boxMessages, __) {
+          final resolved = boxMessages.values
+              .where((e) => e.isResolved)
+              .toList(growable: false);
+          resolved.sort((a, b) => b.timestamp.compareTo(a.timestamp));
+          return resolved.isEmpty
+              ? Center(
+                  child: Text(
+                    'You don’t have any resolved messages',
+                    textAlign: TextAlign.center,
+                    style: textStyleSourceSansPro414,
+                    softWrap: true,
+                  ),
+                )
+              : ListView(
+                  children: resolved
+                      .map((e) => Padding(
+                            padding: paddingV6,
+                            child: MessageListTile(message: e),
+                          ))
+                      .toList(),
+                );
+        },
+      );
+}
