799096f6472f3d762111fb6b40d6f9b0367b54cb
Postnikov Aleksander <alexandrim0@gmail.com>
2023-02-12 06:51:09 +0300

    - cosmetic

--- lib/src/core/adapter/hive_adapter.dart
+++ lib/src/core/adapter/hive_adapter.dart
@@ -2,19 +2,6 @@ import 'package:hive_flutter/hive_flutter.dart';
 
 import '/src/core/model/core_model.dart';
 
-class SettingsModelAdapter extends TypeAdapter<SettingsModel> {
-  @override
-  final typeId = SettingsModel.typeId;
-
-  @override
-  SettingsModel read(BinaryReader reader) =>
-      SettingsModel.fromBytes(reader.readByteList());
-
-  @override
-  void write(BinaryWriter writer, SettingsModel obj) =>
-      writer.writeByteList(obj.toBytes());
-}
-
 class SecretShardModelAdapter extends TypeAdapter<SecretShardModel> {
   @override
   final typeId = SecretShardModel.typeId;
@@ -25,6 +12,19 @@ class SecretShardModelAdapter extends TypeAdapter<SecretShardModel> {
 
   @override
   void write(BinaryWriter writer, SecretShardModel obj) =>
+      writer.writeByteList(obj.toBytes());
+}
+
+class RecoveryGroupModelAdapter extends TypeAdapter<RecoveryGroupModel> {
+  @override
+  final typeId = RecoveryGroupModel.typeId;
+
+  @override
+  RecoveryGroupModel read(BinaryReader reader) =>
+      RecoveryGroupModel.fromBytes(reader.readByteList());
+
+  @override
+  void write(BinaryWriter writer, RecoveryGroupModel obj) =>
       writer.writeByteList(obj.toBytes());
 }
 
@@ -41,15 +41,34 @@ class MessageModelAdapter extends TypeAdapter<MessageModel> {
       writer.writeByteList(obj.toBytes());
 }
 
-class RecoveryGroupModelAdapter extends TypeAdapter<RecoveryGroupModel> {
+class SettingsModelAdapter extends TypeAdapter<SettingsModel> {
   @override
-  final typeId = RecoveryGroupModel.typeId;
+  final typeId = SettingsModel.typeId;
 
   @override
-  RecoveryGroupModel read(BinaryReader reader) =>
-      RecoveryGroupModel.fromBytes(reader.readByteList());
+  SettingsModel read(BinaryReader reader) =>
+      SettingsModel.fromBytes(reader.readByteList());
 
   @override
-  void write(BinaryWriter writer, RecoveryGroupModel obj) =>
+  void write(BinaryWriter writer, SettingsModel obj) =>
       writer.writeByteList(obj.toBytes());
+}
+
+extension SettingsModelExt on Box<SettingsModel> {
+  SettingsModel readWhole() => get(0) ?? const SettingsModel();
+  Future<void> writeWhole(SettingsModel settings) => put(0, settings);
+
+  String get passCode => readWhole().passCode;
+  String get deviceName => readWhole().deviceName;
+  bool get isBiometricsEnabled => readWhole().isBiometricsEnabled;
+  bool get isProxyEnabled => readWhole().isProxyEnabled;
+
+  set passCode(String value) =>
+      writeWhole(readWhole().copyWith(passCode: value));
+  set deviceName(String value) =>
+      writeWhole(readWhole().copyWith(deviceName: value));
+  set isBiometricsEnabled(bool value) =>
+      writeWhole(readWhole().copyWith(isBiometricsEnabled: value));
+  set isProxyEnabled(bool value) =>
+      writeWhole(readWhole().copyWith(isProxyEnabled: value));
 }
