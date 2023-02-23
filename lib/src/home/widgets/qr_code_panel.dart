14bf171e9ff388a5ba8543aba7800659aac8be19
Postnikov Aleksander <alexandrim0@gmail.com>
2023-02-23 14:42:56 +0300

    - add download link

--- lib/src/home/widgets/qr_code_panel.dart
+++ lib/src/home/widgets/qr_code_panel.dart
@@ -1,3 +1,5 @@
+import 'package:share_plus/share_plus.dart';
+
 import '/src/core/theme/theme.dart';
 import '/src/core/widgets/common.dart';
 
@@ -46,6 +48,20 @@ class QRCodePanel extends StatelessWidget {
                     builder: (_) => const QRCodePage(),
                   ),
                 ),
+              ),
+            ),
+            Padding(
+              padding: paddingTop20,
+              child: OutlinedButton(
+                onPressed: () async {
+                  final box = context.findRenderObject() as RenderBox?;
+                  await Share.share(
+                    'https://myguardian.network/app-router',
+                    sharePositionOrigin:
+                        box!.localToGlobal(Offset.zero) & box.size,
+                  );
+                },
+                child: const Text('Share download link'),
               ),
             ),
           ],
