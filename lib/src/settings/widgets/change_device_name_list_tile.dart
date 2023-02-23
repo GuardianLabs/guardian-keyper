f1899ce94b86570d688a1f0d29177ea0fe71d572
Postnikov Aleksander <alexandrim0@gmail.com>
2023-02-23 11:09:05 +0300

    - update device name on changed

--- lib/src/settings/widgets/change_device_name_list_tile.dart
+++ lib/src/settings/widgets/change_device_name_list_tile.dart
@@ -1,3 +1,4 @@
+import '/src/core/model/core_model.dart';
 import '/src/core/di_container.dart';
 import '/src/core/theme/theme.dart';
 import '/src/core/widgets/common.dart';
@@ -9,16 +10,21 @@ class ChangeDeviceNameListTile extends StatelessWidget {
   const ChangeDeviceNameListTile({super.key});
 
   @override
-  Widget build(BuildContext context) => ListTile(
-        leading: const IconOf.shardOwner(),
-        title: const Text('Change Guardian name'),
-        subtitle: Text(
-          context.read<DIContainer>().myPeerId.name,
-          style: textStyleSourceSansPro414Purple,
+  Widget build(BuildContext context) =>
+      ValueListenableBuilder<Box<SettingsModel>>(
+        valueListenable: context.read<DIContainer>().boxSettings.listenable(),
+        builder: (context, settings, __) => ListTile(
+          leading: const IconOf.shardOwner(),
+          title: const Text('Change Guardian name'),
+          subtitle: Text(
+            settings.deviceName,
+            // context.read<DIContainer>().myPeerId.name,
+            style: textStyleSourceSansPro414Purple,
+          ),
+          trailing: const Icon(Icons.arrow_forward_ios_rounded),
+          onTap: () => Navigator.of(context).push(MaterialPageRoute(
+            builder: (_) => const ScaffoldWidget(child: SetDeviceNamePage()),
+          )),
         ),
-        trailing: const Icon(Icons.arrow_forward_ios_rounded),
-        onTap: () => Navigator.of(context).push(MaterialPageRoute(
-          builder: (_) => const ScaffoldWidget(child: SetDeviceNamePage()),
-        )),
       );
 }
