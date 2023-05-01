a1c65df2521161e6f95c089442f1eb83644c4924
Postnikov Aleksander <alexandrim0@gmail.com>
2023-05-02 01:48:02 +0300

    - decompose

--- lib/src/vaults/domain/vault_interactor.dart
+++ lib/src/vaults/domain/vault_interactor.dart
@@ -2,13 +2,13 @@ import 'package:get_it/get_it.dart';
 
 import 'package:guardian_keyper/src/core/data/network_manager.dart';
 import 'package:guardian_keyper/src/core/data/platform_manager.dart';
-import 'package:guardian_keyper/src/core/data/analytics_manager.dart';
 import 'package:guardian_keyper/src/core/domain/entity/core_model.dart';
 import 'package:guardian_keyper/src/settings/data/settings_manager.dart';
 
 import '../data/vault_repository.dart';
+import 'vault_analytics_mixin.dart';
 
-class VaultInteractor {
+class VaultInteractor with VaultAnalyticsMixin {
   late final watch = _vaultRepository.watch;
 
   late final pingPeer = _networkManager.pingPeer;
@@ -48,7 +48,7 @@ class VaultInteractor {
   }
 
   Future<VaultId> removeVault(final VaultId vaultId) async {
-    await GetIt.I<VaultRepository>().delete(vaultId.asKey);
+    await _vaultRepository.delete(vaultId.asKey);
     return vaultId;
   }
 
@@ -89,39 +89,8 @@ class VaultInteractor {
         message: message.copyWith(peerId: _settingsManager.selfId),
       );
 
-  Future<void> logStartCreateVault() =>
-      _analyticsManager.logEvent(eventStartCreateVault);
-
-  Future<void> logFinishCreateVault() =>
-      _analyticsManager.logEvent(eventFinishCreateVault);
-
-  Future<void> logStartAddGuardian() =>
-      _analyticsManager.logEvent(eventStartAddGuardian);
-
-  Future<void> logFinishAddGuardian() =>
-      _analyticsManager.logEvent(eventFinishAddGuardian);
-
-  Future<void> logStartRestoreVault() =>
-      _analyticsManager.logEvent(eventStartRestoreVault);
-
-  Future<void> logFinishRestoreVault() =>
-      _analyticsManager.logEvent(eventFinishRestoreVault);
-
-  Future<void> logStartAddSecret() =>
-      _analyticsManager.logEvent(eventStartAddSecret);
-
-  Future<void> logFinishAddSecret() =>
-      _analyticsManager.logEvent(eventFinishAddSecret);
-
-  Future<void> logStartRestoreSecret() =>
-      _analyticsManager.logEvent(eventStartRestoreSecret);
-
-  Future<void> logFinishRestoreSecret() =>
-      _analyticsManager.logEvent(eventFinishRestoreSecret);
-
   final _networkManager = GetIt.I<NetworkManager>();
   final _platformManager = GetIt.I<PlatformManager>();
   final _settingsManager = GetIt.I<SettingsManager>();
-  final _analyticsManager = GetIt.I<AnalyticsManager>();
   final _vaultRepository = GetIt.I<VaultRepository>();
 }
