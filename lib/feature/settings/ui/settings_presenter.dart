b829ed16bea8b839c37ddf08e8123055dfb4c50f
Postnikov Aleksander <alexandrim0@gmail.com>
2023-12-23 03:47:02 +0300

    - fix: prevent set too short device name

--- lib/feature/settings/ui/settings_presenter.dart
+++ lib/feature/settings/ui/settings_presenter.dart
@@ -1,7 +1,5 @@
-import 'package:get_it/get_it.dart';
 import 'package:flutter/material.dart';
 
-import 'package:guardian_keyper/consts.dart';
 import 'package:guardian_keyper/ui/utils/theme_mode_mapper.dart';
 
 import 'package:guardian_keyper/feature/auth/data/auth_manager.dart';
@@ -26,8 +24,6 @@ class SettingsPresenter extends ChangeNotifier with ThemeModeMapper {
 
   final _networkManager = GetIt.I<NetworkManager>();
 
-  late String _deviceName = _networkManager.selfId.name;
-
   bool _hasBiometrics = false;
 
   bool get hasBiometrics => _hasBiometrics;
@@ -38,21 +34,8 @@ class SettingsPresenter extends ChangeNotifier with ThemeModeMapper {
 
   bool get isBootstrapEnabled => _networkManager.isBootstrapEnabled;
 
-  bool get hasMinimumDeviceNameLength => _deviceName.length >= minNameLength;
-
   ThemeMode get selectedThemeMode =>
       mapBoolToThemeMode(_themeModeHandler.isDarkMode);
-
-  set deviceName(String value) {
-    _deviceName = value;
-    notifyListeners();
-  }
-
-  Future<void> setDeviceName() async {
-    if (_networkManager.selfId.name == _deviceName) return;
-    await _networkManager.setDeviceName(_deviceName);
-    notifyListeners();
-  }
 
   Future<void> setBootstrap(bool isEnabled) async {
     await _networkManager.setBootstrap(isEnabled: isEnabled);
