4c5f31195d925f87adb9540751b9b58008f20c1a
Postnikov Aleksander <alexandrim0@gmail.com>
2024-01-06 18:11:55 +0300

    - add widgets

---
+++ lib/ui/widgets/step_indicator.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class StepIndicator extends StatelessWidget {
+  const StepIndicator({
+    required this.step,
+    required this.stepsCount,
+    super.key,
+  });
+
+  final int step;
+  final int stepsCount;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
