fc2712ae7f03a105a6b4009c959e77ddd006d1c6
Dmitrii <dmitrsosnin@gmail.com>
2024-01-24 21:14:47 +0700

    - update warning when moving a safe

--- lib/feature/vault/ui/_shard_show/dialogs/on_become_owner_dialog.dart
+++ lib/feature/vault/ui/_shard_show/dialogs/on_become_owner_dialog.dart
@@ -1,46 +1,111 @@
-import 'package:guardian_keyper/ui/widgets/common.dart';
-import 'package:guardian_keyper/ui/widgets/icon_of.dart';
+import 'dart:async';
 
-import 'package:guardian_keyper/feature/vault/domain/entity/vault_id.dart';
+import 'package:guardian_keyper/feature/vault/domain/entity/vault.dart';
+import 'package:guardian_keyper/ui/widgets/common.dart';
+import 'package:guardian_keyper/ui/theme/brand_colors.dart';
 import 'package:guardian_keyper/feature/vault/domain/use_case/vault_interactor.dart';
 
-class OnBecomeOwnerDialog extends StatelessWidget {
+class OnBecomeOwnerDialog extends StatefulWidget {
   static Future<void> show(
     BuildContext context, {
-    required VaultId vaultId,
+    required Vault vault,
   }) =>
       showModalBottomSheet(
         context: context,
         isDismissible: true,
         isScrollControlled: true,
-        builder: (_) => OnBecomeOwnerDialog(vaultId: vaultId),
+        builder: (_) => OnBecomeOwnerDialog(vault: vault),
       );
 
   const OnBecomeOwnerDialog({
-    required this.vaultId,
-    super.key,
+    required this.vault,
+    super.key, 
   });
 
-  final VaultId vaultId;
+  final Vault vault;
+
+  @override
+  OnBecomeOwnerDialogState createState() => OnBecomeOwnerDialogState();
+}
+
+class OnBecomeOwnerDialogState extends State<OnBecomeOwnerDialog> {
+  late final _theme = Theme.of(context);
+
+  late final _brandColors = _theme.extension<BrandColors>();
+
+  late final Timer _timer = Timer.periodic(
+    const Duration(seconds: 1),
+    (timer) {
+      if (_countdown > 0) {
+        setState(() {
+          _countdown--;
+        });
+      } else {
+        _timer.cancel();
+      }
+    },
+  );
+
+  int _countdown = 10;
+
+  @override
+  void initState() {
+    super.initState();
+    _timer.hashCode;
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) => BottomSheetWidget(
-        icon: const IconOf.moveVault(size: 80),
-        titleString: 'Moving the Safe',
-        textString: 'By taking this action, the Safe will be transferred to '
-            'your device, and you will gain ownership of it. This process is '
-            'similar to recovering a Safe.',
+        icon: Container(
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: _theme.colorScheme.primary,
+          ),
+          width: 80,
+          height: 80,
+          child: Icon(
+            Icons.warning,
+            color: _brandColors?.warningColor,
+            size: 40,
+          ),
+        ),
+        titleString: 'Irreversible Action Ahead!',
+        textString: 'Once you tap the confirm button, you will no longer be a Guardian '
+            'for ${widget.vault.ownerId.name} and won’t be able to assist with the Safe anymore. '
+            '\n\nInstead, the Safe will be moved to this device in an inactive state — its Secrets '
+            'won’t be accessible until you get enough approvals from the Safe’s Guardians.'
+            '\n\nThis action cannot be undone.',
+
         footer: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             FilledButton(
-              child: const Text('Proceed'),
-              onPressed: () async {
-                await GetIt.I<VaultInteractor>().takeVaultOwnership(vaultId);
-                if (context.mounted) {
-                  Navigator.of(context).popUntil((r) => r.isFirst);
-                }
-              },
+              child: _countdown > 0
+                  ? Text('I want to move the Safe ($_countdown)')
+                  : const Text('Confirm'),
+              style: _theme.filledButtonTheme.style?.copyWith(
+                    backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                      (states) => states.contains(MaterialState.disabled)
+                          ? _brandColors!.dangerColor.withOpacity(0.5)
+                          : _brandColors!.dangerColor,
+                    ),
+                  ),
+              onPressed: _countdown > 0
+                  ? null
+                  : () async {
+                      await GetIt.I<VaultInteractor>()
+                          .takeVaultOwnership(widget.vault.id);
+                      if (context.mounted) {
+                        Navigator.of(context)
+                            .popUntil((r) => r.settings.name == '/');
+                      }
+                    },
             ),
             const Padding(padding: paddingT20),
             FilledButton(
