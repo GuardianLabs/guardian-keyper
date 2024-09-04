af73f1c4cf49dfb9c2b437c84e71d323c884bac7
Dmitrii <dmitrsosnin@gmail.com>
2024-09-04 19:44:32 +0700

    chore: refactor ThemeModeSwitcher to use SettingsPresenter instead of SettingsRepository

--- lib/feature/settings/ui/widgets/theme_mode_switcher.dart
+++ lib/feature/settings/ui/widgets/theme_mode_switcher.dart
@@ -1,21 +1,14 @@
+import 'package:flutter/material.dart';
 import 'package:guardian_keyper/ui/presenters/settings_presenter.dart';
-import 'package:guardian_keyper/ui/widgets/common.dart';
-import 'package:guardian_keyper/data/repositories/settings_repository.dart';
 
 class ThemeModeSwitcher extends StatelessWidget {
   const ThemeModeSwitcher({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final settingsRepository = GetIt.I<SettingsRepository>();
-    return StreamBuilder(
-      initialData: settingsRepository.get<bool>(
-        PreferencesKeys.keyIsDarkModeOn,
-      ),
-      stream: settingsRepository
-          .watch<bool>(PreferencesKeys.keyIsDarkModeOn)
-          .map((event) => event.value),
-      builder: (context, snapshot) {
+    return Selector<SettingsPresenter, bool?>(
+      selector: (_, presenter) => presenter.isDarkModeOn,
+      builder: (context, isDarkModeOn, child) {
         return SegmentedButton<bool?>(
           style: ButtonStyle(
             backgroundColor: WidgetStateProperty.resolveWith<Color?>(
@@ -44,11 +37,20 @@ class ThemeModeSwitcher extends StatelessWidget {
           showSelectedIcon: false,
           emptySelectionAllowed: false,
           multiSelectionEnabled: false,
-          selected: {snapshot.data},
+          selected: {isDarkModeOn},
           onSelectionChanged: (values) async {
-            await context
-                .read<SettingsPresenter>()
-                .setIsDarkModeOn(values.first);
+            final selectedValue = values.first;
+            if (selectedValue == null) {
+              final isSystemDarkMode =
+                  MediaQuery.of(context).platformBrightness == Brightness.dark;
+              await context
+                  .read<SettingsPresenter>()
+                  .setIsDarkModeOn(isSystemDarkMode);
+            } else {
+              await context
+                  .read<SettingsPresenter>()
+                  .setIsDarkModeOn(selectedValue);
+            }
           },
         );
       },
